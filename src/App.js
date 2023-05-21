3b9ed572e59b2fcc734659eb36cca41bcf455936
Mesan <negokamgaing@gmail.com>
2023-05-21 04:51:42 +0100

    update menu-item and use of nagvigate hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import { 
+  Route,
+  Routes, 
+  createBrowserRouter,
+  RouterProvider,
+  Link
+ } from 'react-router-dom'
+
 import './App.css';
 import HomePage from './pages/HomePage.component';
 
+const HatsPage = () => {
+  return(
+    <div>
+      <h1>HatsPage</h1>
+      <button > Direct </button>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {path: "*", Component: Root},
+  {path: "/", Component: HomePage, },
+  {path: "/shop/hats", Component: HatsPage,},
+  ]
+) 
 
 function App() {
+  return <RouterProvider router= {router} />
+
+}
+
+function Root() {
   return (
     <div className="App">
-      <HomePage />
+      
     </div>
   );
 }
